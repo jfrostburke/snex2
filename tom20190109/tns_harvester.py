3622d35de949578bdb6996ca6aa8d212639e51d9
Jamie Burke <jburke@padawan.lco.gtn>
2019-01-14 22:40:08 +0000

    Changing ra/dec conversion to astropy

--- tom20190109/tns_harvester.py
+++ tom20190109/tns_harvester.py
@@ -4,6 +4,8 @@ import os
 import requests
 import json
 from collections import OrderedDict
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 def get(term):
   api_key = os.environ['SNEXBOT_APIKEY']
@@ -25,18 +27,6 @@ def get(term):
   except Exception as e:
     return [None,'Error message : \n'+str(e)]
 
-def convert_radec(ra,dec):
-    
-    [hr,mi,se] = [float(x) for x in ra.split(':')]
-    ra_hr = hr + mi/60. + se/3600.
-    ra_deg = (ra_hr/24.)*360.
-    
-    [de,mi,se] = [abs(float(x)) for x in dec.split(':')]
-    dec_deg = de + mi/60. + se/3600.
-    if '-' in dec: dec_deg = -dec_deg
-    
-    return ra_deg, dec_deg
-
 class TNSHarvester(AbstractHarvester):
     name = 'TNS'
 
@@ -48,6 +38,7 @@ class TNSHarvester(AbstractHarvester):
         target.type = 'SIDEREAL'
         target.identifier = (self.catalog_data['name_prefix'] +
             self.catalog_data['name'])
-        target.ra, target.dec = convert_radec(
-            self.catalog_data['ra'], self.catalog_data['dec'])
+        c = SkyCoord('{0} {1}'.format(self.catalog_data['ra'], 
+            self.catalog_data['dec']), unit = (u.hourangle, u.deg))
+        target.ra, target.dec = c.ra.deg, c.dec.deg
         return target
