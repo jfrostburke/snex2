85f31a7cd656f465a6b3ebff7a3a590a8cda67e3
Jamie Burke <jburke@padawan.lco.gtn>
2019-02-01 00:40:12 +0000

    Fixing migrations so maybe it works on heroku

--- tom_dataproducts/migrations/0002_auto_20190130_2328.py
+++ tom_dataproducts/migrations/0002_auto_20190130_2328.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('data_type', models.CharField(choices=[('SPECTROSCOPY', 'Spectroscopy'), ('PHOTOMETRY', 'Photometry')], default='', max_length=100)),
-                ('timestamp', models.FloatField(db_index=True)),
+                ('timestamp', models.DateTimeField(db_index=True)),
                 ('value', models.FloatField()),
                 ('label', models.CharField(default='', max_length=100)),
                 ('error', models.FloatField(null=True)),
