2c9943bdeb5dbdc1d4d743e9fb25a031633c7096
Jamie Burke <jburke@padawan.lco.gtn>
2019-05-24 02:07:47 +0000

    Improvements to TNS page: fixed recent mag, added LND time filter

--- custom_code/filters.py
+++ custom_code/filters.py
@@ -1,13 +1,20 @@
 from custom_code.models import TNSTarget
 import django_filters
+from django.db.models import Q
+from astropy.time import Time
+from datetime import datetime
 
 class TNSTargetFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name',lookup_expr='icontains')
+    source_group = django_filters.CharFilter(field_name='source_group',lookup_expr='icontains')
+    lnd_jd = django_filters.NumberFilter(field_name='lnd_jd', method='filter_lnd_jd')
+
+    def filter_lnd_jd(self, queryset, name, value):
+        jd_now = Time(datetime.utcnow()).jd
+        return queryset.filter(
+            Q(lnd_jd__gt=jd_now-float(value))
+        )
+
     class Meta:
         model = TNSTarget
-        fields = ['name']
-
-    def __init__(self, *args, **kwargs):
-        super(TNSTargetFilter, self).__init__(*args, **kwargs)
-        if self.data == {}:
-            self.queryset = self.queryset.none()
+        fields = []
--- custom_code/views.py
+++ custom_code/views.py
@@ -17,10 +17,9 @@ def make_coords(ra, dec):
     coords = coords.to_string('hmsdms',sep=':',precision=1,alwayssign=True)
     return coords
 
-def make_lnd(mag, filt, jd):
+def make_lnd(mag, filt, jd, jd_now):
     if not jd:
         return 'Archival'
-    jd_now = Time(datetime.utcnow()).jd
     diff = jd_now - jd
     lnd = '{mag:.2f} ({filt}: {time:.2f} days ago)'.format(
         mag = mag,
@@ -28,16 +27,19 @@ def make_lnd(mag, filt, jd):
         time = diff)
     return lnd
 
-def make_magrecent(all_phot):
-    all_phot = all_phot.replace("'","\"")
-    #print(all_phot)
-    """
-    #Not working right now - fix json ingestion
+def make_magrecent(all_phot, jd_now):
     all_phot = json.loads(all_phot)
-    recent_phot = [obs for obs in all_phot if obs['jd'] ==
-        max([x['jd'] for x in all_phot])][0]
-    """
-    return 'filler'
+    recent_jd = max([all_phot[obs]['jd'] for obs in all_phot])
+    recent_phot = [all_phot[obs] for obs in all_phot if
+        all_phot[obs]['jd'] == recent_jd][0]
+    mag = float(recent_phot['flux'])
+    filt = recent_phot['filters']['name']
+    diff = jd_now - float(recent_jd)
+    mag_recent = '{mag:.2f} ({filt}: {time:.2f} days ago)'.format(
+        mag = mag,
+        filt = filt,
+        time = diff)
+    return mag_recent
 
 class TNSTargets(FilterView):
 
@@ -45,14 +47,17 @@ class TNSTargets(FilterView):
     
     template_name = 'custom_code/tns_targets.html'
     model = TNSTarget
+    paginate_by = 5
+    context_object_name = 'tnstargets'
+    strict = False
     filterset_class = TNSTargetFilter
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        jd_now = Time(datetime.utcnow()).jd
         for target in context['object_list']:
             target.coords = make_coords(target.ra, target.dec)
             target.mag_lnd = make_lnd(target.lnd_maglim,
-                target.lnd_filter, target.lnd_jd)
-            target.mag_recent = make_magrecent(target.all_phot)
+                target.lnd_filter, target.lnd_jd, jd_now)
+            target.mag_recent = make_magrecent(target.all_phot, jd_now)
         return context
