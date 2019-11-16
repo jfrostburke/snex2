c70775f46ae40fb1ea8344c4b344f3ff49ca0590
jfrostburke <jburke@lco.global>
2019-11-16 01:22:45 +0000

    WIP: Adding spectra for Gemini

--- custom_code/facilities/gemini_facility.py
+++ custom_code/facilities/gemini_facility.py
@@ -37,12 +37,12 @@ def get_site_code_from_program(program_id):
     return program_id.split('-')[0]
 
 
-class SNExGeminiObservationForm(GenericObservationForm):
+class OpticalImagingForm(GenericObservationForm):
 
-    window_size = forms.FloatField(label='', initial=1.0, min_value=0.0)
+    window_size = forms.FloatField(initial=1.0, min_value=0.0, label='')
     max_airmass = forms.FloatField(min_value=1.0, max_value=5.0, initial=1.6, label='')
 
-    ##Optical imaging
+    # Optical imaging
     optical_phot_exptime_choices = (
         (0, 0.0),
         (100, 100.0),
@@ -114,10 +114,10 @@ class SNExGeminiObservationForm(GenericObservationForm):
 
         payload = {
             'ready': str(not wait).lower(),
-            'prog': os.environ['GEMINI_PROGRAMID'],
-            'email': os.environ['GEMINI_EMAIL'],
-            'password': os.environ['GEMINI_PASSWORD'],
-            'group': '{name} optical imaging'.format(name=target.name),
+            'prog': os.getenv('GEMINI_PROGRAMID',''),
+            'email': os.getenv('GEMINI_EMAIL',''),
+            'password': os.getenv('GEMINI_PASSWORD',''),
+            'group': 'API Test Optical Imaging',
             'ra': coords.ra.to_string(unit=u.hour,sep=':'),
             'dec': coords.dec.to_string(unit=u.degree,sep=':'),
             'mags': '18.0/g/AB',
@@ -127,7 +127,7 @@ class SNExGeminiObservationForm(GenericObservationForm):
             'elevationType': 'airmass',
             'elevationMin': 1.0,
             'elevationMax': str(self.data['max_airmass']).strip(),
-            'note': 'No finder chart at the moment, sorry',
+            'note': 'API Test',
             'posangle': 90.
         }  
 
@@ -150,8 +150,7 @@ class SNExGeminiObservationForm(GenericObservationForm):
             if float(self.data[f + '_exptime']) > 0.0:
                 payloads[f] = self._init_observation_payload(target)
                 payloads[f]['obsnum'] = obsid_map[f]
-                payloads[f]['target'] = '{name} GMOS {f}'.format(
-                    name=target.name, f=f)
+                payloads[f]['target'] = 'API Test'
                 payloads[f]['exptime'] = self.data[f + '_exptime']
 
         #Need group in there when I'm doing it for real
@@ -160,18 +159,150 @@ class SNExGeminiObservationForm(GenericObservationForm):
         return payloads
 
 
+class OpticalSpectraForm(GenericObservationForm):
+    window_size = forms.FloatField(initial=1.0, min_value=0.0, label='')
+    max_airmass = forms.FloatField(min_value=1.0, max_value=5.0, initial=1.6, label='')
+
+    optical_spec_exptime_choices = (
+        (0, 0.0),
+        (1200, 1200.0),
+        (1500, 1500.0),
+        (1700, 1700.0),
+    )
+
+    optical_spec_slit_choices = (
+        (1, '1.0"'),
+        (1.5, '1.5"'),
+    )
+
+    slit = forms.ChoiceField(choices=optical_spec_slit_choices, initial=1, widget=forms.Select(), required=True,
+                                  label='')
+    b_exptime = forms.ChoiceField(choices=optical_spec_exptime_choices, initial=0, widget=forms.Select(), required=True,
+                                  label='')
+    r_exptime = forms.ChoiceField(choices=optical_spec_exptime_choices, initial=0, widget=forms.Select(), required=True,
+                                  label='')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper.layout = Layout(
+            self.common_layout,
+            Div(
+                Div(
+                    Div(HTML("<p></p>"),
+                        PrependedAppendedText(
+                            'window_size', 'Once in the next', 'days'
+                        ),
+                        PrependedText(
+                            'max_airmass', 'Airmass <'
+                        ),
+                        Div(
+                            Div(HTML('<p style="text-align:center;">Grating</p>'), css_class='col-md-2'),
+                            Div(HTML('<p style="text-align:center;">Exposure time (s)</p>'), css_class='col-md-10'),
+                            css_class='form-row',
+                        ),
+                        Div(
+                            Div(HTML('<p style="text-align:center;">B600/500nm</p>'), css_class='col-md-2'),
+                            Div('b_exptime', css_class='col-md-10'), css_class='form-row'
+                        ),
+                        Div(
+                            Div(HTML('<p style="text-align:center;">R400/850nm</p>'), css_class='col-md-2'),
+                            Div('r_exptime', css_class='col-md-10'), css_class='form-row'
+                        ),
+                        Div(
+                            Div(HTML('<p style="text-align:center;">Slit</p>'), css_class='col-md-12'),
+                            css_class='form-row',
+                        ),
+                        Div(
+                            Div('slit', css_class='col-md-12'), css_class='form-row'
+                        ),
+                        ), css_class='col-md-8'
+                ), css_class='row justify-content-md-center'
+            )
+        )
+
+    def is_valid(self):
+        super().is_valid()
+        errors = GeminiFacility.validate_observation(self.observation_payload())
+        if errors:
+            self.add_error(None, errors)
+        return not errors
+
+    def _init_observation_payload(self, target):
+        wait = True #On Hold
+        coords = SkyCoord(ra=target.ra*u.degree, dec=target.dec*u.degree)
+        now = datetime.utcnow()
+        sn_name = target.name
+
+        payload = {
+            'ready': str(not wait).lower(),
+            'prog': os.getenv('GEMINI_PROGRAMID',''),
+            'email': os.getenv('GEMINI_EMAIL',''),
+            'password': os.getenv('GEMINI_PASSWORD',''),
+            'group': 'API Test Optical Spectra (B600)',
+            'ra': coords.ra.to_string(unit=u.hour,sep=':'),
+            'dec': coords.dec.to_string(unit=u.degree,sep=':'),
+            'mags': '18.0/g/AB',
+            'windowDate': now.strftime('%Y-%m-%d'),
+            'windowTime': now.strftime('%H:%M:%S'),
+            'windowDuration': int(float(self.data['window_size'])*24),
+            'elevationType': 'airmass',
+            'elevationMin': 1.0,
+            'elevationMax': str(self.data['max_airmass']).strip(),
+            'note': 'API Test',
+            'posangle': 90.
+        }
+
+        return payload
+
+    def observation_payload(self):
+
+        target = Target.objects.get(pk=self.data['target_id'])
+
+        # Starting with 1.5", B600 to test
+        obsid_map = {
+            'arc': '47',
+            'acquisition': '48',
+            'science_with_flats': '49',
+        }
+
+        payloads = {}
+
+        for key in obsid_map:
+            payloads[key] = self._init_observation_payload(target)
+            payloads[key]['obsnum'] = obsid_map[key]
+            if key == 'arc':
+                payloads[key]['target'] = 'CuAr Arc'
+            elif key == 'acquisition':
+                payloads[key]['target'] = 'Acquisition'
+            elif key == 'science_with_flats':
+                payloads[key]['target'] = 'Science with Flats'
+                payloads[key]['exptime'] = self.data['b_exptime']
+
+        # Need group in there when I'm doing it for real
+        # Not sure what to do about mags for now
+
+        return payloads
+
+
 class GeminiFacility(GenericObservationFacility):
     name = 'Gemini'
-    form = SNExGeminiObservationForm
-    observation_types = [('IMAGING', 'Optical Imaging')]
+    observation_types = [
+        ('IMAGING_OPTICAL', 'Optical Imaging'),
+        ('SPECTRA_OPTICAL', 'Optical Spectra')
+    ]
 
-    def get_form(self, observation_type):
-        return self.form
+    def get_form(self, observation_type='IMAGING_OPTICAL'):
+        if observation_type == 'IMAGING_OPTICAL':
+            return OpticalImagingForm
+        elif observation_type == 'SPECTRA_OPTICAL':
+            return OpticalSpectraForm
+        else:
+            return OpticalImagingForm
 
     @classmethod
     def submit_observation(clz, observation_payloads):
 
-        server = os.environ['GEMINI_SERVER']
+        server = os.getenv('GEMINI_SERVER','')
         url = server + '/too'
 
         new_observation_ids = []
