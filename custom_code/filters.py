ae39b3ca699aaf79ef1b53e733097ffc3e92036a
crpellegrino <cpellegrino@lco.global>
2020-09-08 11:23:14 -0700

    Added target list filtering by science tags

--- custom_code/filters.py
+++ custom_code/filters.py
@@ -1,4 +1,4 @@
-from custom_code.models import TNSTarget
+from custom_code.models import TNSTarget, ScienceTags, TargetTags
 from tom_targets.models import Target, TargetList#
 from tom_targets.filters import filter_for_field#
 from django.conf import settings
@@ -6,6 +6,7 @@ import django_filters
 from django.db.models import ExpressionWrapper, FloatField, Q
 from math import radians
 from django.db.models.functions.math import ACos, Cos, Radians, Pi, Sin
+from django.db.models.functions import Lower
 from astropy.time import Time
 from datetime import datetime
 from django import forms
@@ -79,6 +80,7 @@ class CustomTargetFilter(django_filters.FilterSet):
             new_filter = filter_for_field(field)
             new_filter.parent = self
             self.filters[field['name']] = new_filter
+        self.filters['sciencetags'].field.label_from_instance = lambda obj: obj.tag
 
     name = django_filters.CharFilter(method='filter_name', label='Name')
 
@@ -143,7 +145,13 @@ class CustomTargetFilter(django_filters.FilterSet):
 
     targetlist__name = django_filters.ModelChoiceFilter(queryset=get_target_list_queryset, label=    "Target Grouping")
 
+    sciencetags = django_filters.ModelChoiceFilter(queryset=ScienceTags.objects.all().order_by(Lower('tag')), label="Science Tag", method='filter_sciencetags')
+
+    def filter_sciencetags(self, queryset, name, value):
+        #tag_id = ScienceTags.objects.filter(tag=value).first().id
+        return queryset.filter(targettags__tag=value).distinct()
+
     class Meta:
         model = Target
         #fields = ['type', 'name', 'key', 'value', 'cone_search', 'targetlist__name']
-        fields = ['name', 'cone_search', 'targetlist__name']
+        fields = ['name', 'cone_search', 'targetlist__name', 'sciencetags']
