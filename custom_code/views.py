c395fa63081d7272cd8b438467de936a019f2607
crpellegrino <cpellegrino@lco.global>
2020-09-14 14:32:43 -0700

    Added async plot loading to target list page

--- custom_code/views.py
+++ custom_code/views.py
@@ -1,7 +1,7 @@
 from django_filters.views import FilterView
-from django.shortcuts import redirect #
+from django.shortcuts import redirect, render
 from django.db.models import Q #
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from custom_code.models import TNSTarget, ScienceTags, TargetTags
 from custom_code.filters import TNSTargetFilter, CustomTargetFilter #
@@ -16,6 +16,12 @@ from astropy.time import Time
 from datetime import datetime
 from datetime import timedelta
 import json
+
+from plotly import offline
+import plotly.graph_objs as go
+from tom_dataproducts.models import ReducedDatum
+from django.utils.safestring import mark_safe
+from custom_code.templatetags.custom_code_tags import get_24hr_airmass
 
 # Create your views here.
 
@@ -174,3 +180,145 @@ def save_target_tag_view(request):
     response_data = {'success': 1}
     return HttpResponse(json.dumps(response_data), content_type='application/json')
 
+
+def targetlist_collapse_view(request):
+
+    target_id = request.GET.get('target_id', None)
+    target = Target.objects.get(id=target_id)
+
+    def lightcurve_collapse_view(target):
+        def get_color(filter_name):
+            filter_translate = {'U': 'U', 'B': 'B', 'V': 'V',
+                'g': 'g', 'gp': 'g', 'r': 'r', 'rp': 'r', 'i': 'i', 'ip': 'i',
+                'g_ZTF': 'g_ZTF', 'r_ZTF': 'r_ZTF', 'i_ZTF': 'i_ZTF', 'UVW2': 'UVW2', 'UVM2': 'UVM2',
+                'UVW1': 'UVW1'}
+            colors = {'U': 'rgb(59,0,113)',
+                'B': 'rgb(0,87,255)',
+                'V': 'rgb(120,255,0)',
+                'g': 'rgb(0,204,255)',
+                'r': 'rgb(255,124,0)',
+                'i': 'rgb(144,0,43)',
+                'g_ZTF': 'rgb(0,204,255)',
+                'r_ZTF': 'rgb(255,124,0)',
+                'i_ZTF': 'rgb(144,0,43)',
+                'UVW2': '#FE0683',
+                'UVM2': '#BF01BC',
+                'UVW1': '#8B06FF',
+                'other': 'rgb(0,0,0)'}
+            try: color = colors[filter_translate[filter_name]]
+            except: color = colors['other']
+            return color
+    
+        photometry_data = {}
+        for rd in ReducedDatum.objects.filter(target=target, data_type='photometry'):
+            value = json.loads(rd.value)
+            photometry_data.setdefault(value.get('filter', ''), {})
+            photometry_data[value.get('filter', '')].setdefault('time', []).append(rd.timestamp)
+            photometry_data[value.get('filter', '')].setdefault('magnitude', []).append(value.get('magnitude',None))
+            photometry_data[value.get('filter', '')].setdefault('error', []).append(value.get('error', None))
+        plot_data = [
+            go.Scatter(
+                x=filter_values['time'],
+                y=filter_values['magnitude'], mode='markers',
+                marker=dict(color=get_color(filter_name)),
+                #name=filter_name,
+                error_y=dict(
+                    type='data',
+                    array=filter_values['error'],
+                    visible=True,
+                    color=get_color(filter_name)
+                )
+            ) for filter_name, filter_values in photometry_data.items()]
+        layout = go.Layout(
+            xaxis=dict(gridcolor='#D3D3D3',showline=True,linecolor='#D3D3D3',mirror=True),
+            yaxis=dict(autorange='reversed',gridcolor='#D3D3D3',showline=True,linecolor='#D3D3D3',mirror=True),
+            margin=dict(l=30, r=10, b=30, t=40),
+            hovermode='closest',
+            height=200,
+            width=250,
+            showlegend=False,
+            plot_bgcolor='white'
+        )
+        if plot_data:
+            return offline.plot(go.Figure(data=plot_data, layout=layout), output_type='div', show_link=False, config={'staticPlot': True}, include_plotlyjs=False) 
+        else:
+            return 'No photometry for this target yet.'
+
+    def spectra_collapse_view(target):
+        spectra = []
+        spectral_dataproducts = ReducedDatum.objects.filter(target=target, data_type='spectroscopy')
+        for spectrum in spectral_dataproducts:
+            datum = json.loads(spectrum.value)
+            wavelength = []
+            flux = []
+            for key, value in datum.items():
+                wavelength.append(value['wavelength'])
+                flux.append(float(value['flux']))
+            spectra.append((wavelength, flux))
+        plot_data = [
+            go.Scatter(
+                x=spectrum[0],
+                y=spectrum[1]
+            ) for spectrum in spectra]
+        layout = go.Layout(
+            height=200,
+            width=250,
+            margin=dict(l=30, r=10, b=30, t=40),
+            showlegend=False,
+            xaxis=dict(
+                gridcolor='#D3D3D3',
+                showline=True,
+                linecolor='#D3D3D3',
+                mirror=True
+            ),
+            yaxis=dict(
+                showticklabels=False,
+                gridcolor='#D3D3D3',
+                showline=True,
+                linecolor='#D3D3D3',
+                mirror=True
+            ),
+            plot_bgcolor='white'
+        )
+        if plot_data:
+            return offline.plot(go.Figure(data=plot_data, layout=layout), output_type='div', show_link=False, config={'staticPlot': True}, include_plotlyjs='cdn')
+        else:
+            return 'No spectra for this target yet.'
+
+    def airmass_collapse_view(target):
+        interval = 30 #min
+        airmass_limit = 3.0
+
+        obj = Target
+        obj.ra = target.ra
+        obj.dec = target.dec
+        obj.epoch = 2000
+        obj.type = 'SIDEREAL'
+
+        plot_data = get_24hr_airmass(obj, interval, airmass_limit)
+        layout = go.Layout(
+            xaxis=dict(gridcolor='#D3D3D3',showline=True,linecolor='#D3D3D3',mirror=True),
+            yaxis=dict(range=[airmass_limit,1.0],gridcolor='#D3D3D3',showline=True,linecolor='#D3D3D3',mirror=True),
+            margin=dict(l=20,r=10,b=30,t=40),
+            hovermode='closest',
+            width=250,
+            height=200,
+            showlegend=False,
+            plot_bgcolor='white'
+        )
+        visibility_graph = offline.plot(
+                go.Figure(data=plot_data, layout=layout), output_type='div', show_link=False, config={'staticPlot': True}, include_plotlyjs='cdn'
+        )
+        return visibility_graph
+
+    lightcurve_plot = lightcurve_collapse_view(target)
+    spectra_plot = spectra_collapse_view(target)
+    airmass_plot = airmass_collapse_view(target)
+
+    context = {
+        'lightcurve_plot': lightcurve_plot,
+        'spectra_plot': spectra_plot,
+        'airmass_plot': airmass_plot
+    }
+
+    return HttpResponse(json.dumps(context), content_type='application/json')
