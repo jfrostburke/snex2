2c0dd0043722dc73ddaabb1306a4a3dbe32574a3
crpellegrino <cpellegrino@lco.global>
2020-09-20 09:46:50 -0700

    Fixed custom target filter to inherit built-in target filter

--- custom_code/filters.py
+++ custom_code/filters.py
@@ -1,6 +1,6 @@
 from custom_code.models import TNSTarget, ScienceTags, TargetTags
-from tom_targets.models import Target, TargetList#
-from tom_targets.filters import filter_for_field#
+from tom_targets.models import Target, TargetList
+from tom_targets.filters import filter_for_field, TargetFilter
 from django.conf import settings
 import django_filters
 from django.db.models import ExpressionWrapper, FloatField, Q
@@ -70,9 +70,7 @@ class TNSTargetFilter(django_filters.FilterSet):
         fields = []
         form = TNSTargetForm
 
-class CustomTargetFilter(django_filters.FilterSet):
-    #key = django_filters.CharFilter(field_name='targetextra__key', label='Key')
-    #value = django_filters.CharFilter(field_name='targetextra__value', label='Value')
+class CustomTargetFilter(TargetFilter):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -82,76 +80,14 @@ class CustomTargetFilter(django_filters.FilterSet):
             self.filters[field['name']] = new_filter
         self.filters['sciencetags'].field.label_from_instance = lambda obj: obj.tag
 
-    name = django_filters.CharFilter(method='filter_name', label='Name')
-
-    def filter_name(self, queryset, name, value):
-        return queryset.filter(Q(name__icontains=value) | Q(aliases__name__icontains=value)).distinct()
-
-    cone_search = django_filters.CharFilter(method='filter_cone_search', label='Cone Search',
-                                            help_text='RA, Dec, Search Radius (degrees)')
-
-    target_cone_search = django_filters.CharFilter(method='filter_cone_search', label='Cone Search (Target)',
-                                                   help_text='Target Name, Search Radius (degrees)')
-
-    def filter_cone_search(self, queryset, name, value):
-        if name == 'cone_search':
-            ra, dec, radius = value.split(',')
-        elif name == 'target_cone_search':
-            target_name, radius = value.split(',')
-            targets = Target.objects.filter(
-                Q(name__icontains=target_name) | Q(aliases__name__icontains=target_name)
-            ).distinct()
-            if len(targets) == 1:
-                ra = targets[0].ra
-                dec = targets[0].dec
-            else:
-                return queryset.filter(name=None)
-
-        ra = float(ra)
-        dec = float(dec)
-
-        double_radius = float(radius) * 2
-        queryset = queryset.filter(ra__gte=ra - double_radius, ra__lte=ra + double_radius,
-                                   dec__gte=dec - double_radius, dec__lte=dec + double_radius)
-
-        separation = ExpressionWrapper(
-            180 * ACos(
-                (Sin(radians(dec)) * Sin(Radians('dec'))) +
-                (Cos(radians(dec)) * Cos(Radians('dec')) * Cos(radians(ra) - Radians('ra')))
-            ) / Pi(), FloatField()
-        )
-
-        return queryset.annotate(separation=separation).filter(separation__lte=radius)
-#        half_pi = 90
-#
-#        separation = ExpressionWrapper(
-#            ACos(
-#                (Cos(half_pi - float(dec)) * Cos(half_pi - F('dec'))) +
-#                (Sin(half_pi - float(dec)) * Sin(half_pi - F('dec')) * Cos(float(ra) - F('ra')))
-#            ), FloatField()
-#        )
-#
-#        return queryset.annotate(separation=separation).filter(separation__lte=radius)
-
-    def filter_target_cone_search(self, queryset, name, value):
-        return queryset
-
-    # hide target grouping list if user not logged in
-    def get_target_list_queryset(request):
-        if request.user.is_authenticated:
-            return TargetList.objects.all()
-        else:
-            return TargetList.objects.none()
-
-    targetlist__name = django_filters.ModelChoiceFilter(queryset=get_target_list_queryset, label=    "Target Grouping")
+    key = None
+    value = None
 
     sciencetags = django_filters.ModelChoiceFilter(queryset=ScienceTags.objects.all().order_by(Lower('tag')), label="Science Tag", method='filter_sciencetags')
 
     def filter_sciencetags(self, queryset, name, value):
-        #tag_id = ScienceTags.objects.filter(tag=value).first().id
         return queryset.filter(targettags__tag=value).distinct()
 
     class Meta:
         model = Target
-        #fields = ['type', 'name', 'key', 'value', 'cone_search', 'targetlist__name']
         fields = ['name', 'cone_search', 'targetlist__name', 'sciencetags']
