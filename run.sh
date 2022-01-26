fc7c79ef9309215ac4223e3003301cd1f95c1046
crpellegrino <cpellegrino@lco.global>
2022-01-26 09:24:40 -0800

    Bumping up number of workers to improve performance

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python manage.py migrate --noinput
-gunicorn -b 0.0.0.0:8080 snex2.wsgi --access-logfile - --error-logfile - -k gevent --timeout 300 --workers 2
+gunicorn -b 0.0.0.0:8080 snex2.wsgi --access-logfile - --error-logfile - -k gevent --timeout 300 --workers 4
