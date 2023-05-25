0e2efc9ece7ec2818eda436d0df5b7c973fc505c
crpellegrino <cpellegrino@lco.global>
2023-05-25 13:27:41 -0700

    Changed name of o4 targets in snex1

--- gw/hooks.py
+++ gw/hooks.py
@@ -80,30 +80,30 @@ def ingest_gw_galaxy_into_snex1(target_id, event_id, wrapped_session=None):
     else:
         db_session = _return_session(_snex1_address)
 
-    o4_targets = _load_table('o4_targets', db_address=_snex1_address)
+    o4_galaxies = _load_table('o4_galaxies', db_address=_snex1_address)
 
-    existing_target = db_session.query(o4_targets).filter(o4_targets.target_id==target_id)
+    existing_target = db_session.query(o4_galaxies).filter(o4_galaxies.target_id==target_id)
     if existing_target.count() > 0:
         if any([t.superevent_id == event_id for t in existing_target]):
-            logger.info('Already ingested target {} into o4_targets table for event {}'.format(target_id, event_id))
+            logger.info('Already ingested target {} into o4_galaxies table for event {}'.format(target_id, event_id))
 
         else:
-            logger.info('Found existing target {} in o4_targets table, copying it'.format(target_id))
+            logger.info('Found existing target {} in o4_galaxies table, copying it'.format(target_id))
             existing_target_row = existing_target.first()
-            existing_table = existing_target_row.__table__ #Somehow this is different than the o4_targets object
+            existing_table = existing_target_row.__table__ #Somehow this is different than the o4_galaxies object
             
             non_pk_columns = [k for k in existing_table.columns.keys() if k not in existing_table.primary_key.columns.keys()]
             data = {c: getattr(existing_target_row, c) for c in non_pk_columns}
             data['superevent_id'] = event_id
 
-            db_session.add(o4_targets(**data))
+            db_session.add(o4_galaxies(**data))
 
     else:
         snex2_target = Target.objects.get(id=target_id)
         ra0 = snex2_target.ra
         dec0 = snex2_target.dec
 
-        db_session.add(o4_targets(target_id=target_id, superevent_id=event_id, ra0=ra0, dec0=dec0))
+        db_session.add(o4_galaxies(target_id=target_id, superevent_id=event_id, ra0=ra0, dec0=dec0))
 
     if not wrapped_session:
         try:
@@ -116,4 +116,4 @@ def ingest_gw_galaxy_into_snex1(target_id, event_id, wrapped_session=None):
     else:
         db_session.flush()
 
-    logger.info('Finished ingesting target {} into o4_targets'.format(target_id))
+    logger.info('Finished ingesting target {} into o4_galaxies'.format(target_id))
