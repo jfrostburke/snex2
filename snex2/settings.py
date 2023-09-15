e93b74b1208b4b841f799975b30f8f00ee45b4ce
crpellegrino <cpellegrino@lco.global>
2023-09-15 14:48:02 -0400

    Django 4.2 changed how storage objects are configured

--- snex2/settings.py
+++ snex2/settings.py
@@ -228,8 +228,10 @@ MEDIA_URL = '/data/'
 # Using AWS
 
 if not DEBUG:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
+
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRECT_ACCESS_KEY', '')
