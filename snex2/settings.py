1485e22329e915cfaa7a2be3d4dd72e7449fe29e
crpellegrino <cpellegrino@lco.global>
2023-09-18 16:14:26 -0400

    Typo

--- snex2/settings.py
+++ snex2/settings.py
@@ -253,7 +253,7 @@ if not DEBUG:
 
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRECT_ACCESS_KEY', '')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 
 LOGGING = {
     'version': 1,
